05b020f24d21d660c108558364bae46f64de3287
siby <simonlobito2006@gmail.com>
2024-12-09 13:40:20 -0500

    arreglo icono

--- src/components/CarruselRecetas.jsx
+++ src/components/CarruselRecetas.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import recetas from '../models/recetas.json'
 import Receta from './Receta'
+import '../styles/carrusel-recetas.css'
+import { GiCampCookingPot } from 'react-icons/gi'
 
 export default function CarruselRecetas() {
   return (
-    <div style={{display:"flex",gap:"10px",flexWrap:"wrap",width:"80%",margin:'auto',justifyContent:'center'}}>
-        {recetas.map(receta=><Receta receta={receta} />)}
+    <div className='contenedor-recetas'>
+      <div style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
+        <h2 className='titulo-recetas'>Recetas</h2>
+        <GiCampCookingPot className='receta-icono' />
+      </div>
+      <div className='contenedor-carrusel-recetas'>
+          {recetas.map(receta=><Receta receta={receta} />)}
+      </div>
     </div>
   )
 }
