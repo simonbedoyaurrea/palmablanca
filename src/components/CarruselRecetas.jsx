b4a58c2fb63e18a44101321a5c08df7e82d47a52
siby <simonlobito2006@gmail.com>
2025-01-26 13:51:48 -0500

    mas recetas

--- src/components/CarruselRecetas.jsx
+++ src/components/CarruselRecetas.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import recetas from '../models/recetas.json'
 import Receta from './Receta'
 import '../styles/carrusel-recetas.css'
@@ -6,6 +6,9 @@ import { GiCampCookingPot } from 'react-icons/gi'
 
 
 export default function CarruselRecetas() {
+
+  const [masRecetas,setMasRecetas]=useState(false)
+
   return (
     <div className='contenedor-recetas'>
       <div style={{display:'flex',justifyContent:'center',alignItems:'center',width:'100vw',boxSizing:'border-box'}}>
@@ -13,8 +16,9 @@ export default function CarruselRecetas() {
         <GiCampCookingPot className='receta-icono' />
       </div>
       <div className='contenedor-carrusel-recetas'>
-          {recetas.map(receta=><Receta receta={receta} />)}
+          {recetas.slice(0,masRecetas?recetas.length:4).map(receta=><Receta receta={receta} />)}
       </div>
+      {recetas.length>5?<button className='boton-ver-mas' onClick={()=>setMasRecetas(!masRecetas)} >{masRecetas?'ver menos':'ver más recetas'}</button>:null}
     </div>
   )
 }
--- src/components/Receta.jsx
+++ src/components/Receta.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/receta.css'
 import { CiClock1 } from 'react-icons/ci'
+import { BiBook } from 'react-icons/bi'
+import { FaYoutube } from 'react-icons/fa'
+
+
+
 
 export default function Receta({receta}) {
+
+  let iconoFuente
+
+  switch (receta.plataforma) {
+    case "texto":
+      iconoFuente=<BiBook />
+      break;
+      
+    case "youtube":
+      iconoFuente=<FaYoutube/>
+      break;
+    default:
+      break;
+  }
   return (
         <div className='contenedor-receta'>
             <a target='_blank' href={receta.link} >
             <img className='imagen-receta' src={receta.imagen} alt={receta.nombre} />
             <div className='info-receta'>
                 <h3 className='nombre-receta'>{receta.nombre}</h3>
-                <p>{receta.tiempo }<CiClock1/></p>
+                <div className='info-receta-tiempo-fuente'>
+                  <p style={{display:'flex',alignItems:'center'}}><CiClock1/>{receta.tiempo }</p>
+                  <div>{iconoFuente}</div>
+                </div>
             </div>
             </a>
         </div>
